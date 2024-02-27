876d1c498fa546b7bb08f8c71ad8ca646a51e496
Mikkel Jürs <s224279@dtu.dk>
2024-02-27 08:36:05 +0100

    Updated with SpringBoot dependencies.

---
+++ src/main/java/com/othelloai/aiapi/OthelloAPI.java
@@ -0,0 +1,11 @@
+package com.othelloai.aiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OthelloAPI {
+    public static void main(String[] args) {
+        SpringApplication.run(OthelloAPI.class, args);
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.othelloai.aiapi {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+
+
     opens com.othelloai.aiapi to javafx.fxml;
     exports com.othelloai.aiapi;
 }
