522ecc4fae997cc2df4b158d74e0f65d84a3c6da
Mikkel Jürs <s224279@dtu.dk>
2024-03-12 11:30:32 +0100

    Added response 400 if false

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,8 +8,10 @@ module com.othelloai.aiapi {
     requires spring.context;
     requires spring.web;
     requires java.sql;
+    requires java.desktop;
 
     opens com.othelloai.aiapi to javafx.fxml, spring.core;
     opens com.othelloai.aiapi.repository to spring.beans;
+    opens com.othelloai.aiapi.controller to spring.beans, spring.context, spring.web; // This line is added
     exports com.othelloai.aiapi;
 }
