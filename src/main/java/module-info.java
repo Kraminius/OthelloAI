a49949a66791d3bcced09fa67fff904a212684f3
mikkelJurs <s224279@dtu.dk>
2024-03-18 21:54:20 +0100

    Error in pom.xml

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -11,13 +11,10 @@ module com.othelloai.aiapi {
     requires java.desktop;
     requires com.google.gson;
 
-    // Existing opens directives
     opens com.othelloai.aiapi to javafx.fxml, spring.core;
     opens com.othelloai.aiapi.repository to spring.beans;
     opens com.othelloai.aiapi.controller to spring.beans, spring.context, spring.web;
-
-    // Add this line to open the package containing BoardResponse to Gson
-    opens com.othelloai.aiapi.model to com.google.gson; // Adjust the package name as necessary
+    opens com.othelloai.aiapi.model to com.google.gson;
 
     exports com.othelloai.aiapi;
 }
