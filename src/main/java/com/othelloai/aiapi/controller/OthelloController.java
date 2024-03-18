b729b89a299600d71713c9db0409e06c69008fbe
mikkelJurs <s224279@dtu.dk>
2024-03-18 21:27:13 +0100

    Fixed typo.

--- src/main/java/com/othelloai/aiapi/controller/OthelloController.java
+++ src/main/java/com/othelloai/aiapi/controller/OthelloController.java
@@ -272,9 +272,16 @@ public class OthelloController {
     public void updateAIProgress(int progress, int end, boolean player) {
         String text = progress + " of " + end;
         System.out.println(text);
-        if (Config.getTurn() && (Config.getGameType().equals(GameType.PLAYER_VS_AI))) {right.setAIProgressLabel(text);  if(progress > (0.9*end)) right.setAIProgressLabel("Waiting...");}
-        else if (!player && (Config.getGameType().equals(GameType.AI_VS_AI))) {left.setAIProgressLabel(text); if(progress >= (0.9*end)) {left.setAIProgressLabel("Waiting...");}}
-        else if (player && (Config.getGameType().equals(GameType.AI_VS_AI))) {right.setAIProgressLabel(text);  if(progress >= (0.9*end)) right.setAIProgressLabel("Waiting...");}
+        if (Config.getTurn() && (Config.getGameType().equals(GameType.PLAYER_VS_AI))) {right.setAIProgressLabel(text); right.setAIProgressLabel("Waiting...");}
+        else if (!player && (Config.getGameType().equals(GameType.AI_VS_AI))) left.setAIProgressLabel(text);
+        else if (player && (Config.getGameType().equals(GameType.AI_VS_AI))) right.setAIProgressLabel(text);
+
+        if(progress >= (0.9*end) && (!player)) {
+            left.setAIProgressLabel("Waiting...");
+        }
+        if (progress >= (0.9*end) && player){
+            right.setAIProgressLabel("Waiting...");
+        }
     }
 
     private Piece[][] getPiecesFromIntArr(int[][] array){
